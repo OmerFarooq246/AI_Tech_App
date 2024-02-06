4c20520bf93f6df1eace18c5e58b7bb09d1cde32
Omer Farooq <ofarooq.bscs21seecs@seecs.edu.pk>
2024-02-06 20:18:00 +0500

    FrontEnd: login, signup Done

---
+++ pages/dashboard.js
@@ -0,0 +1,10 @@
+import Dashboard from "@/components/Dashboard/Dashboard"
+import BaseLayout from "@/components/BaseLayout/BaseLayout"
+
+export default function Login(){
+    return(
+        <BaseLayout title={"Login"}>
+            <Dashboard />
+        </BaseLayout>
+    )
+}
