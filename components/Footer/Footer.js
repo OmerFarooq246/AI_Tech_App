3b4f157beae273ef3a40bd87bfc353eefe6b144c
Omer Farooq <ofarooq.bscs21seecs@seecs.edu.pk>
2024-02-07 16:52:57 +0500

    added products fetched from db

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,8 +1,8 @@
 export default function Footer(){
     return(
-        <footer class="text-sm flex flex-row justify-between items-center h-10 pl-3.5 pr-5">
-            <p class="text-xs w-1/3">Copyright - AI Tech App - 2024 All rights Unreserved</p>
-            <div class="space-x-2.5 w-1/3 flex flex-row justify-end items-center">
+        <footer className="text-sm flex flex-row justify-between items-center h-10 pl-3.5 pr-5">
+            <p className="text-xs w-1/3">Copyright - AI Tech App - 2024 All rights Unreserved</p>
+            <div className="space-x-2.5 w-1/3 flex flex-row justify-end items-center">
                 <a href="">Home</a>
                 <a href="">Dashboard</a>
                 <a href="">About</a>
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -2,6 +2,7 @@ import Dashboard from "@/components/Dashboard/Dashboard"
 import BaseLayout from "@/components/BaseLayout/BaseLayout"
 
 export default function Login(){
+    
     return(
         <BaseLayout title={"Login"}>
             <Dashboard />
